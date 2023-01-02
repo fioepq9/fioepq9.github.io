f255b8d3ca15a610915c85b726419c80407d5e1b
fino <fioepq9@qq.com>
2023-01-02 16:14:39 +0800

    chore: hexo deploy

---
+++ scripts/19.删除链表的倒数第-N-个结点/solution_test.go
@@ -0,0 +1,37 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+// 19.删除链表的倒数第N个结点
+// https://leetcode.cn/problems/remove-nth-node-from-end-of-list
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+
+}
+func TestSolution(t *testing.T) {
+	testCases := []struct {
+		desc string
+		want 
+	}{
+		{
+            want: ,
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			get := 
+			if !reflect.DeepEqual(tC.want,get){
+				t.Errorf("input: %+v get: %v\n",tC,get)
+			}
+		})
+	}
+}
